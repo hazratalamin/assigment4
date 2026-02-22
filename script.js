d9bd41c117edb6ffd34e999c104009922bd7f1ea
hazratalamin <alamin.islan23333@gmail.com>
2026-02-23 02:07:29 +0600

    interview level complete

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let rejectedList =[];
 
 let total = document.getElementById('total');
 let interviewCount = document.getElementById('interviewCount');
-let  rejectCount = document.getElementById('rejectCount');
+let  rejectCount = document.getElementById('rejectedCount');
 
 const allFilterBtn = document.getElementById('all-filter-btn');
 const interviewFilterBtn = document.getElementById('interview-filter-btn');
@@ -13,7 +13,8 @@ const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
 const allCards = document.getElementById('allCards');
 
 const mainContainer = document.querySelector('main');
-console.log(mainContainer);
+
+const filteredCards = document.getElementById('filteredCards');
 
 
 
@@ -38,4 +39,71 @@ console.log(id);
 
   selected.classList.remove('bg-gray-300', 'text-white');
   selected.classList.add('bg-[#3B82F6]', 'text-white')
+}
+
+mainContainer.addEventListener('click', function(event){
+
+    console.log(event.target.classList.contains('interview-btn'));
+
+        if(event.target.classList.contains('interview-btn')){
+
+    const parentNode = event.target.parentNode.parentNode;
+    
+   
+
+    
+    const title = parentNode.querySelector('h1').innerText;
+    const role = parentNode.querySelectorAll('p')[0].innerText;
+    const details = parentNode.querySelectorAll('p')[1].innerText;
+    const status = parentNode.querySelector('.status').innerText;
+
+    const cardInfo = {
+        title,
+        role,
+        details,
+        status
+    }
+    const titleExist = interviewList.find(item => item.title === cardInfo.title);
+if(!titleExist){
+    interviewList.push(cardInfo);
+} 
+ renderInterview()
+}
+    
+});
+
+function renderInterview (){
+
+filteredCards.innerHTML = '';
+
+for( let interview of interviewList){
+
+console.log(interview);
+
+let div = document.createElement('div');
+
+div.className ='card flex justify-between border border-gray-200 rounded-[10px] p-5';
+
+div.innerHTML =`   
+<div class="space-y-2">
+<h1>Mobile First Corp </h1>
+<p>React Native Developer</p>
+<p>Remote • Full-time • $130,000 - $175,000</p>
+
+<p class="status">Not Applied</p>
+<p>Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+<!-- btn1 -->
+<div class="space-x-2">
+    <button class=" interview-btn bg-white-500 text-green-500 px-4 py-2 border rounded">INTERVIEW</button>
+    <button class=" rejected-btn bg-white-500 text-red-500 px-4 py-2 border rounded">REJECTED</button>
+</div>
+        </div>
+        <!-- main2 -->
+        <div>
+           <img src="./jobs.png" alt="" class="delete-btn">
+        </div>
+`
+
+}
+
 }
